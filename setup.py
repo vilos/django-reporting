3de4bdfe6911e676aa6f438785cc33b52954124d
Rinat Shigapov <rinatshigapov@gmail.com>
2013-02-06 13:55:35 +0400

    README.TXT -> README.md

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='django-reporting',
     version=__import__('reporting').__version__,
     description='django-reporting',
-    long_description=open('README.TXT').read(),
+    long_description=open('README.md').read(),
     author='Vitaliy Kucheraviy, Rinat Shigapov',
     author_email='ppr.vitaly@gmail.com, rinatshigapov@gmail.com',
     url='https://github.com/DXist/django-reporting',
@@ -16,7 +16,7 @@ setup(
     ],
     #test_suite='debug_toolbar.runtests.runtests',
     include_package_data=True,
-    zip_safe=False, # because we're including media that Django needs
+    zip_safe=False,  # because we're including media that Django needs
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
